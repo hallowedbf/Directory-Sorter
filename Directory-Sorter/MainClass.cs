1e473333403163358115c68a61500a585f36853d
hallowedbf <hallowedbf@gmail.com>
2021-02-22 16:47:49 -0600

    building out the groundwork still

---
+++ Directory-Sorter/MainClass.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Directory_Sorter
+{
+    class MainClass
+    {
+        const string version = "1.0.0";
+        static void Main(string[] args)
+        {
+            Sorting.Initialize();
+            Console.WriteLine($"Directory-Sorter v{version}");
+            Console.WriteLine("___________________________________________");
+            DirectoryQuery();
+        }
+
+        public static void DirectoryQuery()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Choose a directory to sort.");
+            Console.WriteLine("You will get to choose how you want the directory sorted before anything is moved.");
+            Console.WriteLine("1. Desktop | 2. Downloads | 3. Documents | 4. Manually enter full directory path");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    {
+                        SortQuery(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory));
+                        break;
+                    }
+                case ConsoleKey.D2:
+                    {
+                        SortQuery(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads");
+                        break;
+                    }
+                case ConsoleKey.D3:
+                    {
+                        SortQuery(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+                        break;
+                    }
+                case ConsoleKey.D4:
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Enter a valid folder path.");
+                        SortQuery(Console.ReadLine());
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("That's not a 1, 2, 3, or a 4. I am sad now.");
+                        DirectoryQuery();
+                        break;
+                    }
+            }
+        }
+
+        public static void SortQuery(string directoryPath)
+        {
+            if(!Directory.Exists(directoryPath))
+            {
+                Console.WriteLine();
+                Console.WriteLine($"This directory does not exist: {directoryPath}");
+                Console.WriteLine("Try checking for any typos.");
+                DirectoryQuery();
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Directory found: {directoryPath}");
+                Console.WriteLine();
+                Console.WriteLine("What kind of sorting do you want done?");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine("1. Default");
+                Console.WriteLine("For each file in this directory, this will check the file's file extension (.jpg, .mp3, .zip, etc) and attempt to");
+                Console.WriteLine("recognize it from a list of different file types I've hard-coded into the program.");
+                Console.WriteLine();
+                Console.WriteLine("It will take the files it recognizes and try to put them in common sense places like, pictures will go into your");
+                Console.WriteLine("Pictures folder, text files will go into your Documents folder, etc.");
+                Console.WriteLine();
+                Console.WriteLine("If it sees a file that it recognizes and doesn't know where to put it, it will ask you where it put files of that");
+                Console.WriteLine("type from then on. It will also do this if it comes across a file it does not recognize.");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine("2. Isolate");
+                Console.WriteLine("For each file in this directory, this will check the file's file extension (.jpg, .mp3, .zip, etc) and make a");
+                Console.WriteLine("folder only for files with that exact file extension.");
+                Console.WriteLine();
+                Console.WriteLine("This can be useful for isolating all of the different files you have so you can move specific files you are");
+                Console.WriteLine("looking for out of a large mess.");
+                Console.WriteLine();
+                Console.WriteLine("If you have many different file types in this directory, this will make many different folders to look through");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine();
+                Console.WriteLine("3. Go back");
+                switch(Console.ReadKey().Key)
+                {
+                    case ConsoleKey.D1:
+                        {
+                            Sorting.DefaultSort(directoryPath);
+                            break;
+                        }
+                    case ConsoleKey.D2:
+                        {
+                            Sorting.IsolateSort(directoryPath);
+                            break;
+                        }
+                    case ConsoleKey.D3:
+                        {
+                            DirectoryQuery();
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ Directory-Sorter/Sorting.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Directory_Sorter
+{
+    class Sorting
+    {
+        public static List<string> knownFileTypes;
+        public static List<string> imageFileTypes;
+        public static List<string> audioFileTypes;
+        public static List<string> videoFileTypes;
+        public static List<string> shortcutFileTypes;
+        public static void Initialize()
+        {
+            Console.Clear();
+            Console.WriteLine("Initializing...");
+            knownFileTypes = new List<string>();
+            imageFileTypes = new List<string>();
+            audioFileTypes = new List<string>();
+            videoFileTypes = new List<string>();
+            shortcutFileTypes = new List<string>();
+
+            #region Image Files
+            imageFileTypes.Add(".jpg");
+            imageFileTypes.Add(".jpeg");
+            imageFileTypes.Add(".png");
+            imageFileTypes.Add(".bmp");
+            imageFileTypes.Add(".gif");
+            imageFileTypes.Add(".jfif");
+            imageFileTypes.Add(".webp");
+            imageFileTypes.Add(".ppm");
+            imageFileTypes.Add(".pgm");
+            imageFileTypes.Add(".pbm");
+            imageFileTypes.Add(".tiff");
+            imageFileTypes.Add(".pdn");
+            imageFileTypes.Add(".psd");
+            imageFileTypes.Add(".cpt");
+            imageFileTypes.Add(".kra");
+            imageFileTypes.Add(".psp");
+            imageFileTypes.Add(".tga");
+            #endregion
+
+            #region Audio Files
+            audioFileTypes.Add(".aac");
+            audioFileTypes.Add(".act");
+            audioFileTypes.Add(".aiff");
+            audioFileTypes.Add(".flac");
+            audioFileTypes.Add(".m4a");
+            audioFileTypes.Add(".mp3");
+            audioFileTypes.Add(".mpc");
+            audioFileTypes.Add(".nmf");
+            audioFileTypes.Add(".ogg");
+            audioFileTypes.Add(".oga");
+            audioFileTypes.Add(".mogg");
+            audioFileTypes.Add(".opus");
+            audioFileTypes.Add(".org");
+            audioFileTypes.Add(".wav");
+            audioFileTypes.Add(".wma");
+            #endregion
+
+            #region Video Files
+            videoFileTypes.Add(".webm");
+            videoFileTypes.Add(".mkv");
+            videoFileTypes.Add(".flv");
+            videoFileTypes.Add(".ogv");
+            videoFileTypes.Add(".gifv");
+            videoFileTypes.Add(".avi");
+            videoFileTypes.Add(".mov");
+            videoFileTypes.Add(".qt");
+            videoFileTypes.Add(".wmv");
+            videoFileTypes.Add(".amv");
+            videoFileTypes.Add(".mpg");
+            videoFileTypes.Add(".mp2");
+            videoFileTypes.Add(".mpeg");
+            videoFileTypes.Add(".mpe");
+            videoFileTypes.Add(".mpv");
+            videoFileTypes.Add(".m2v");
+            videoFileTypes.Add(".m4v");
+            videoFileTypes.Add(".3gp");
+            videoFileTypes.Add(".3g2");
+            videoFileTypes.Add(".f4v");
+            videoFileTypes.Add(".f4p");
+            videoFileTypes.Add(".f4a");
+            videoFileTypes.Add(".f4b");
+            #endregion
+            #region Shortcut Files
+            shortcutFileTypes.Add(".lnk");
+            shortcutFileTypes.Add(".url");
+            #endregion
+            Console.Clear();
+        }
+
+        public static void DefaultSort(string directoryPath)
+        {
+            foreach (string filePath in Directory.EnumerateFiles(directoryPath))
+            {
+                
+            }
+            Console.ReadKey();
+        }
+
+        public static void IsolateSort(string directoryPath)
+        {
+
+        }
+
+        public static bool IsImage(string filePath)
+        {
+            return true;
+        }
+    }
+}
